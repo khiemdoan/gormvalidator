4dc5690f36ea2bd1a7fa37c8e366618c1c4ea9a9
Khiem Doan <doankhiem.crazy@gmail.com>
2023-02-10 10:02:55 +0700

    Fix bug when using model in model

--- callmethod.go
+++ callmethod.go
@@ -10,16 +10,14 @@ func callMethod(db *gorm.DB, fc func(value interface{}, tx *gorm.DB) bool) {
 	tx := db.Session(&gorm.Session{NewDB: true})
 
 	value := db.Statement.ReflectValue
-	if called := fc(value.Interface(), tx); !called {
-		switch value.Kind() {
-		case reflect.Slice, reflect.Array:
-			db.Statement.CurDestIndex = 0
-			for i := 0; i < value.Len(); i++ {
-				fc(reflect.Indirect(value.Index(i)).Addr().Interface(), tx)
-				db.Statement.CurDestIndex++
-			}
-		case reflect.Struct:
-			fc(value.Addr().Interface(), tx)
+	switch value.Kind() {
+	case reflect.Slice, reflect.Array:
+		db.Statement.CurDestIndex = 0
+		for i := 0; i < value.Len(); i++ {
+			fc(reflect.Indirect(value.Index(i)).Addr().Interface(), tx)
+			db.Statement.CurDestIndex++
 		}
+	case reflect.Struct:
+		fc(value.Addr().Interface(), tx)
 	}
 }
